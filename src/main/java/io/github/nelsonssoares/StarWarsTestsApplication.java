badaf98d8946b49e2972e8c9c749dd01b1dbbcc8
Nelson <punkpunkada@gmail.com>
2023-11-07 13:30:27 -0300

    iniciando projeto

---
+++ src/main/java/io/github/nelsonssoares/StarWarsTestsApplication.java
@@ -0,0 +1,13 @@
+package io.github.nelsonssoares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsTestsApplication.class, args);
+	}
+
+}
